4240ef0c98edc0bba954f61daa977567ff220f6d
KaganMuslu <kaanwasder@gmail.com>
2024-04-15 15:37:35 +0300

    Roller bitti

--- Cafe_App/Areas/Admin/Controllers/RolController.cs
+++ Cafe_App/Areas/Admin/Controllers/RolController.cs
@@ -14,6 +14,24 @@ namespace Cafe_App.Areas.Admin.Controllers
 
         public IActionResult Index()
 		{
+            ViewBag.personelRolGrup = _context.Personeller
+									.Join(_context.Roller, // Roller tablosu ile birleştirme
+										personel => personel.RolId, // Personel tablosundaki RolId alanı
+										rol => rol.Id, // Rol tablosundaki Id alanı
+										(personel, rol) => new {
+											RolId = personel.RolId,
+											RolAdi = rol.Ad,
+											Personel = personel
+										}) // Personel ve Rol tablolarının birleştirilmesi
+									.GroupBy(p => p.RolId)
+									.Select(g => new
+									{
+										RolId = g.Key,
+										RolAdi = g.First().RolAdi, // Grubun ilk elemanının rol adı
+										PersonelListesi = g.Select(p => p.Personel).ToList()
+									})
+									.ToList();
+
 			return View();
 		}
 
@@ -36,5 +54,26 @@ namespace Cafe_App.Areas.Admin.Controllers
 
 			return RedirectToAction("Index");
 		}
+
+		public IActionResult PersonelSil(int Id)
+		{
+			var personel = _context.Personeller.FirstOrDefault(x => x.Id == Id);
+			if (personel != null)
+			{
+				if (personel.Gorunurluk == true)
+				{
+					personel.Gorunurluk = false;
+				}
+				else
+				{
+					personel.Gorunurluk = true;
+				}
+				_context.Update(personel);
+				_context.SaveChanges();
+			}
+
+			return RedirectToAction("Index");
+		}
+
 	}
 }
