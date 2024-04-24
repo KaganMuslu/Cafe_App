7811089c31f5c48b326398a06d11ad4e793ca9c9
KaganMuslu <kaanwasder@gmail.com>
2024-04-24 17:56:06 +0300

    Masa görüntüleme sayfası dolu-boş ve diğer bilgilere göre düzenlendi.

--- Cafe_App/Migrations/20240424140824_init.Designer.cs
+++ Cafe_App/Migrations/20240424140824_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cafe_App.Migrations
 {
     [DbContext(typeof(IdentityDataContext))]
-    [Migration("20240424134807_init")]
+    [Migration("20240424140824_init")]
     partial class init
     {
         /// <inheritdoc />
