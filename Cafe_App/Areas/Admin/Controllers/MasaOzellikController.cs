fb8ed9e027a6e03b97f43251ded96af72d8c374f
KaganMuslu <kaanwasder@gmail.com>
2024-05-10 17:50:38 +0300

    Masa özellik controller'ı sayfası, ekleme işlemi ve güncelleme işlemi eklendi

---
+++ Cafe_App/Areas/Admin/Controllers/MasaOzellikController.cs
@@ -0,0 +1,61 @@
+﻿using Cafe_App.Areas.Admin.Models;
+using Cafe_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cafe_App.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class MasaOzellikController : Controller
+	{
+		private readonly IdentityDataContext _context;
+		public MasaOzellikController(IdentityDataContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var viewModel = new MasaOzellikViewModel
+			{
+				Ozellik = new Ozellik(),
+				MasaOzellik = new MasaOzellik(),
+				Ozellikler = _context.Ozellikler.ToList(),
+				MasaOzellikler = _context.MasaOzellikler.ToList()
+			};
+
+			return View(viewModel);
+		}
+
+		[HttpPost]
+		public IActionResult Index(MasaOzellikViewModel model)
+		{
+			if (model.Ozellik != null)
+			{
+				if (model.Ozellik.Id == null) // Ekle
+				{
+					var ozellik = _context.Ozellikler.FirstOrDefault(x => x.Ad == model.Ozellik.Ad);
+					if (ozellik == null)
+					{
+						_context.Add(model.Ozellik);
+					}
+					else
+					{
+						// Önceki soruguyu untracked yani takipsiz yapma
+						var entry = _context.Entry(ozellik);
+						entry.State = EntityState.Detached;
+
+						_context.Update(model.Ozellik);
+					}
+				}
+				else // Güncelle
+				{
+					_context.Update(model.Ozellik);
+				}
+			}
+
+			_context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ Cafe_App/Areas/Admin/Models/MasaOzellikViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Cafe_App.Models;
+
+namespace Cafe_App.Areas.Admin.Models
+{
+	public class MasaOzellikViewModel
+	{
+		public Ozellik Ozellik { get; set; }
+
+		public MasaOzellik MasaOzellik { get; set; }
+
+		public List<Ozellik> Ozellikler { get; set; }
+
+		public List<MasaOzellik> MasaOzellikler { get; set; }
+	}
+}
