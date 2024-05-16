f6dc3512752cd108aff56db0920352137a78bc5e
KaganMuslu <kaanwasder@gmail.com>
2024-05-16 17:52:41 +0300

    Ürünler sayfasına pagination eklendi

--- Cafe_App/Areas/Musteri/Controllers/MenuController.cs
+++ Cafe_App/Areas/Musteri/Controllers/MenuController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cafe_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cafe_App.Areas.Musteri.Controllers
 {
     [Area("Musteri")]
     public class MenuController : Controller
     {
+        private readonly IdentityDataContext _context;
+        public MenuController(IdentityDataContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var menuler = _context.Menuler.Include(x => x.Kategori).ToList();
+
+            return View(menuler);
         }
     }
 }
--- Cafe_App/Areas/Musteri/Controllers/UrunController.cs
+++ Cafe_App/Areas/Musteri/Controllers/UrunController.cs
@@ -1,6 +1,8 @@
 ﻿using Cafe_App.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Drawing.Printing;
+using X.PagedList;
 
 namespace Cafe_App.Areas.Musteri.Controllers
 {
@@ -13,11 +15,21 @@ namespace Cafe_App.Areas.Musteri.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var urunler = _context.Urunler.Include(x => x.Kategori).ToList();
+            var pageSize = 8;
+            var urunler = _context.Urunler.Include(x => x.Kategori).ToPagedList(page, pageSize);
+
+            var totalCount = _context.Urunler.Count(); // Toplam ürün sayısı
+            var startCount = (page - 1) * pageSize + 1; // Başlangıç sayısı
+            var endCount = Math.Min(startCount + pageSize - 1, totalCount); // Bitiş sayısı
+
+            ViewBag.TotalCount = totalCount;
+            ViewBag.StartCount = startCount;
+            ViewBag.EndCount = endCount;
 
             return View(urunler);
         }
+
     }
 }
